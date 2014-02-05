1585025b12362558290836263de5edafba42d3fa
Jason Dillon <jason@planet57.com>
2014-02-05 15:31:44 -0800

    Add autosubmit task, wasted way too much time trying to debug fucking ID issues.... fuck I hate the nexus-scheduler shit

--- plugins/internal/nexus-analytics-plugin/src/main/java/org/sonatype/nexus/analytics/internal/AutoSubmitTaskDescriptor.java
+++ plugins/internal/nexus-analytics-plugin/src/main/java/org/sonatype/nexus/analytics/internal/AutoSubmitTaskDescriptor.java
@@ -12,38 +12,28 @@
  */
 package org.sonatype.nexus.analytics.internal;
 
-import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Singleton;
 
-import org.sonatype.nexus.analytics.EventExporter;
-import org.sonatype.nexus.scheduling.NexusTaskSupport;
-
-import static com.google.common.base.Preconditions.checkNotNull;
+import org.sonatype.nexus.tasks.descriptors.AbstractScheduledTaskDescriptor;
 
 /**
- * Event submission task.
+ * {@link AutoSubmitTask} descriptor.
  *
  * @since 2.8
  */
-@Named
-public class SubmitTask
-  extends NexusTaskSupport
+@Named(AutoSubmitTask.ID)
+@Singleton
+public class AutoSubmitTaskDescriptor
+  extends AbstractScheduledTaskDescriptor
 {
-  private final EventExporter eventExporter;
-
-  @Inject
-  public SubmitTask(final EventExporter eventExporter) {
-    this.eventExporter = checkNotNull(eventExporter);
+  @Override
+  public String getId() {
+    return AutoSubmitTask.ID;
   }
 
   @Override
-  protected String getMessage() {
-    return "Submitting analytics events";
-  }
-
-  @Override
-  protected void execute() throws Exception {
-    // HACK: for now simply export with drop
-    eventExporter.export(true);
+  public String getName() {
+    return "Analytics: Automatic submission";
   }
 }
